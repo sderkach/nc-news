ae619d37d62453d4e21933b97064ee9d919dc556
Serhii Derkach <serhiiderkach@gmail.com>
2025-03-10 16:59:33 +0000

    Task 2: GET topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,13 @@
 const endpointsJson = require("../endpoints.json");
 const request = require("supertest");
 const app = require("../app");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const db = require("../db/connection");
 
-/* Set up your beforeEach & afterAll functions here */
+
+beforeEach(() => seed(data));
+afterAll(() => db.end());
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -13,4 +18,29 @@ describe("GET /api", () => {
         expect(endpoints).toEqual(endpointsJson);
       });
   });
+});
+
+describe("GET /api/topics", () => {
+  test("200: responds with all topics", () => {
+    return request(app)
+    .get('/api/topics')
+    .expect(200)
+    .then(({ body }) => {
+      expect(body.topics.length).toBe(3);
+      body.topics.forEach(topic => {
+        const { slug, description, img_url } = topic;
+        expect(typeof slug).toBe('string');
+        expect(typeof description).toBe('string');
+        expect(typeof img_url).toBe('string');
+      })
+    })
+  });
+
+  test("404: responds with invalid URl if given an invalid URL", () => {
+    return request(app).get("/api/topicss")
+    .expect(404)
+    .then(({ body }) => {
+        expect(body.msg).toBe('Invalid URL');
+    });
+});
 });
--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const endpoints = require("./endpoints.json");
+const { getTopics } = require("./controllers/topics.controller");
+
 
 app.get("/api", (req, res) => res.status(200).send({endpoints}));
+
+app.get("/api/topics", getTopics);
+
+app.all('*', (req, res, next) => {
+    res.status(404).send({ msg: 'Invalid URL' });
+})
 
 module.exports = app;
